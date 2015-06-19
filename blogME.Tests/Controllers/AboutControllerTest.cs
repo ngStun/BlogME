90fd2c382b4d1ea57af917a0fcb5527bf08d8ac8
adeel azhar <adeel2929@gmail.com>
2015-06-18 20:46:59 -0400

    Updated Microsoft.AspNet.Mvc to latest version 5.2.3. Also added unit test classes in test project to test all the controllers.

---
+++ blogME.Tests/Controllers/AboutControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using blogME.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace blogME.Tests.Controllers
+{
+    [TestClass]
+    public class AboutControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var controller = new AboutController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
