f5ccef881aa6df5ce5db3a3364ccaee365058562
adeel azhar <adeel2929@gmail.com>
2015-07-05 18:02:13 -0400

    Removed the categories table as tags can be used to sort the articles.

--- BlogME.Database/dbo/Tables/Articles.sql
+++ BlogME.Database/dbo/Tables/Articles.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [dbo].[Articles] (
     [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [CategoryId]  INT            NOT NULL,
     [Title]       NVARCHAR (128) NOT NULL,
     [Body]        NTEXT          NOT NULL,
     [CreatedAt]   DATETIME       NOT NULL,
@@ -8,7 +7,8 @@
     [UserId]      INT            NOT NULL,
     [IsPublished] BIT            NOT NULL,
     CONSTRAINT [PK_Articles] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Articles_Categories] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([Id]),
     CONSTRAINT [FK_Articles_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
 );
+
+
 
--- BlogME.Database/dbo/Tables/Categories.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories] (
-    [Id]   INT           IDENTITY (1, 1) NOT NULL,
-    [Name] NVARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
