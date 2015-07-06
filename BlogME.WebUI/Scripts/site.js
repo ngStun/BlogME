8824f3c951a7af93eeb289dfa9c77134b60ef695
adeel azhar <adeel2929@gmail.com>
2015-07-05 21:55:55 -0400

    Updated meta tag in the layout pages to disable zoominng on mobile devices. Refactored the javascript code to add/remove tags on the tags page.

---
+++ BlogME.WebUI/Scripts/site.js
@@ -0,0 +1,19 @@
+﻿// Add listener for tag removals
+$(document).on('click', '.tag-cloud', function () { $(this).remove(); });
+
+$(document).ready(function () {
+    $('#tag > div > div > label').click(function () { addTag('#tag'); });
+    $('#tag > div > div > input').keyup(function (e) { if (e.keyCode === 13) { addTag('#tag'); } });
+});
+
+function addTag(id) {
+    var tag = $(id + ' > div > div > input').val();
+
+    // If there is no value in the input field then don't do anything
+    if (tag !== '') {
+        // Append tag into the tag cloud 
+        $('<li class="tag-cloud">' + tag + '</li>').appendTo("#tag-cloud");
+        // Clear input back to nothing
+        $(id + ' > div > div > input').val('');
+    }
+}
