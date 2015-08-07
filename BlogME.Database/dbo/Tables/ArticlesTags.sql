f196f1c94896673bcc546db1c299e362bf2a38ca
adeel azhar <adeel2929@gmail.com>
2015-08-07 00:04:47 -0400

    Added a column and foreign key for ArticlesTags table

--- BlogME.Database/dbo/Tables/ArticlesTags.sql
+++ BlogME.Database/dbo/Tables/ArticlesTags.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[ArticlesTags] (
     [Id]        INT IDENTITY (1, 1) NOT NULL,
     [ArticleId] INT NOT NULL,
+    [TagId]     INT NOT NULL,
     CONSTRAINT [PK_ArticlesTags] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_ArticlesTags_Articles] FOREIGN KEY ([ArticleId]) REFERENCES [dbo].[Articles] ([Id])
+    CONSTRAINT [FK_ArticlesTags_Articles] FOREIGN KEY ([ArticleId]) REFERENCES [dbo].[Articles] ([Id]),
+    CONSTRAINT [FK_ArticlesTags_Tags] FOREIGN KEY ([Id]) REFERENCES [dbo].[Tags] ([Id])
 );
+
+
 
